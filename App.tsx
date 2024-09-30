ff18b00030654a2084ab8a6a1eca64f293531597
Andrew Browne <3808083+frankenwino@users.noreply.github.com>
2024-09-30 14:20:39 +0200

    Added eslint

--- App.tsx
+++ App.tsx
@@ -1,6 +1,5 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { StyleSheet } from 'react-native';
 import AboutScreen from './screens/AboutScreen';
 import HomeScreen from './screens/HomeScreen';
 
@@ -22,11 +21,11 @@ export default function App() {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
